1990b41ca873d7f3bb71781751fc00304444672e
xenophont <javier.deno@gmail.com>
2023-01-19 00:55:36 +0100

    fix deploy error

--- src/index.js
+++ src/index.js
@@ -148,6 +148,7 @@ root.render(
   </AmplifyProvider>
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
